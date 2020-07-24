117da8895b9a4a22ed55d7f3c35153a8b234340b
Peter Heilbo Ratgen <peter@pratgen.dk>
2020-07-24 15:04:40 +0200

    Completed UI for the feedback submission

--- app/src/main/java/org/faith/bebetter/YouPage/FeedbackActivity.java
+++ app/src/main/java/org/faith/bebetter/YouPage/FeedbackActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.faith.bebetter.R;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class FeedbackActivity extends AppCompatActivity {
 
@@ -12,5 +14,7 @@ public class FeedbackActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feedback);
+        DatabaseReference feedbackDatabase = FirebaseDatabase.getInstance().getReference().child("Feedback").child("Test");
+        feedbackDatabase.setValue("323");
     }
 }
